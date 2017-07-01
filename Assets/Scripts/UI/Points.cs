84f4f8887ab0363c37dbf150ee7fec632615e257
lilyhollow <chanjection@gmail.com>
2017-07-01 05:21:36 -0500

    Added point system

---
+++ Assets/Scripts/UI/Points.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+
+    public static Points instance;
+    private Text pointsTxtRef;
+
+    [SerializeField]
+    private Text pointsText;
+
+    private void Awake()
+    {
+        pointsTxtRef = GetComponent<Text>();
+    }
+    //then where you need:
+    private void Start()
+    {
+        pointsText.text = "0";
+    }
+
+    private void Update()
+    {
+        UpdatePoints();
+
+    }
+
+    public void UpdatePoints()
+    {
+        //fundoRundo = GameManager.instance.fundsArmyOne;
+        pointsText.text = GameManager.instance.totalPoints.ToString();
+    }
+}
