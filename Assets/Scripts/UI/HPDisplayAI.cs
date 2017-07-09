85145d663b459fa68d123521180bdebc17a4aac8
lilyhollow <chanjection@gmail.com>
2017-07-09 05:01:05 -0500

    Cleaned up code

--- Assets/Scripts/UI/HPDisplayAI.cs
+++ Assets/Scripts/UI/HPDisplayAI.cs
@@ -11,18 +11,18 @@ public class HPDisplayAI : MonoBehaviour
     public AIPlayerFix thisAIUnit = instance.transform.parent.transform.GetComponent<AIPlayerFix>();
 
     [SerializeField]
-    private Text txtRef;
+    private Text HPTxtRef;
 
     private void Awake()
     {
-        txtRef = GetComponent<Text>();//or provide from somewhere else (e.g. if you want via find GameObject.Find("CountText").GetComponent<Text>();)
+        HPTxtRef = GetComponent<Text>();//or provide from somewhere else (e.g. if you want via find GameObject.Find("CountText").GetComponent<Text>();)
         //unitRef = GetComponent<Unit>();
     }
     //then where you need:
     private void Start()
     {
         HPForDisplay = 10;
-        txtRef.text = "";
+        HPTxtRef.text = "";
     }
 
     private void Update()
@@ -37,26 +37,9 @@ public class HPDisplayAI : MonoBehaviour
 
     public void UpdateHPIndicator()
     {
-        if (thisAIUnit.HP < 10 && thisAIUnit.HP > 0)
-        {
-            if (thisAIUnit.HP == 1)
-                txtRef.text = "1";
-            else if (thisAIUnit.HP == 2)
-                txtRef.text = "2";
-            else if (thisAIUnit.HP == 3)
-                txtRef.text = "3";
-            else if (thisAIUnit.HP == 4)
-                txtRef.text = "4";
-            else if (thisAIUnit.HP == 5)
-                txtRef.text = "5";
-            else if (thisAIUnit.HP == 6)
-                txtRef.text = "6";
-            else if (thisAIUnit.HP == 7)
-                txtRef.text = "7";
-            else if (thisAIUnit.HP == 8)
-                txtRef.text = "8";
-            else if (thisAIUnit.HP == 9)
-                txtRef.text = "9";
-        }
+
+        HPForDisplay = thisAIUnit.HP;
+        if (thisAIUnit.HP < 10)
+            HPTxtRef.text = HPForDisplay.ToString();
     }
 }
